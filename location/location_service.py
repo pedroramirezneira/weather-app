4f60f50b01a5b08af5a0055ded71d2581284bd63
Pedro Ramirez Neira <pedroramirezneira@gmail.com>
2025-10-01 20:37:12 -0300

    feat: add xoaco

---
+++ location/location_service.py
@@ -0,0 +1,21 @@
+from concurrent import futures
+import grpc
+import service_pb2
+import service_pb2_grpc
+import requests
+
+class LocationService(service_pb2_grpc.LocationServicer):
+    def GetLocation(self, request, context):
+        response = requests.get("https://ipwho.is/")
+        data = response.json()
+        return service_pb2.Point(data["latitude"], data["longitude"])
+
+def serve(port: int = 3030):
+        server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        service_pb2_grpc.add_LocationServicer_to_server(LocationService(), server)
+        server.add_insecure_port("localhost:{port}")
+        server.start()
+        server.wait_for_termination()
+
+if __name__ == "__main__":
+    serve()
